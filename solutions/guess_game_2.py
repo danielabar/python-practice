812007c542e9b7ca17916c6ede90b5bdef38cab2
Daniela Baron <baron.daniela@gmail.com>
2016-11-27 14:26:14 -0500

    detect past end of guessing range

--- solutions/guess_game_2.py
+++ solutions/guess_game_2.py
@@ -1,4 +1,8 @@
 import sys
+import logging
+
+MIN = 1
+MAX = 100
 
 
 def make_guess(low, high):
@@ -7,26 +11,29 @@ def make_guess(low, high):
 
 
 def get_guess(guess):
-    return input("Is your number {}? Answer y(es) / l(ow) / h(igh)? ".format(guess))
+    return input("Is your number {}? Answer y(es) / l(ow) / h(igh) / q(uit)? ".format(guess))
 
 
 def guess_game():
     answer = 'y'
     guess_count = 1
-    low = 1
-    high = 100
-    guess = make_guess(1, 100)
+    low = MIN
+    high = MAX
+    guess = make_guess(low, high)
 
     input("Pick a number between 1 and 100 inclusive but do not type it in. Hit enter to continue...")
     previous_answer = -1
     answer = get_guess(guess)
 
     while answer != 'y':
-        # if answer == previous_answer:
-        #     print("liar")
-        #     sys.exit()
-        # else:
-        #     previous_answer = answer
+        if answer == 'q':
+            sys.exit()
+
+        logging.debug("low = {}, high = {}, guess = {}".format(low, high, guess))
+
+        if low > MAX or high < MIN:
+            print("Sorry, this isn't working out. Goodbye.")
+            sys.exit()
 
         guess_count += 1
         if answer == 'l':
@@ -41,4 +48,5 @@ def guess_game():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARN)
     guess_game()
